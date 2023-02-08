9fad3466c28cc76bb855f46f5f7b13510a2c047c
Olamide Opayemi <horlameedeh@gmail.com>
2023-02-08 23:16:16 +0100

    remove extra server and refactor outputs

--- outputs.tf
+++ outputs.tf
@@ -30,16 +30,3 @@ output "max_value" {
 output "min_value" {
   value = local.minimum
 }
-
-# Export attributes from the S3 Bucket
-output "data-bucket-arn" {
-  value = data.aws_s3_bucket.data_bucket.arn
-}
-
-output "data-bucket-domain-name" {
-  value = data.aws_s3_bucket.data_bucket.bucket_domain_name
-}
-
-output "data-bucket-region" {
-  value = "The ${data.aws_s3_bucket.data_bucket.id} bucket is locatedin ${data.aws_s3_bucket.data_bucket.region}"
-}
